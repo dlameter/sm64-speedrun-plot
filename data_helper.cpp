01561a21d091da4f3a5a025d18ae01cba7719044
Dominik Lameter <dominik@lameter.com>
2020-08-06 13:24:58 -0500

    Added isOpen definition.

--- data_helper.cpp
+++ data_helper.cpp
@@ -1,12 +1,24 @@
 #include "data_helper.h"
 
 DataHelper::DataHelper(QString filename) {
+    opened = false;
     QFile file(filename);
 
     if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
         std::cerr << "Failed to open data file!" << std::endl;
+        return;
     }
 
+    QJsonParseError json_error;
+    dataDoc = QJsonDocument::fromJson(file.readAll(), &json_error);
+    if (json_error.error != QJsonParseError::ParseError::NoError) {
+        std::cerr << "DataHelper: Failed to parse JSON file. " << json_error.errorString().toStdString() << std::endl;
+        return;
+    }
 
-    dataDoc = 
+    opened = true;
+}
+
+bool DataHelper::isOpen() {
+    return opened;
 }
