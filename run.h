a88bceed137ab0bf0f64cc577d16884466356723
Dominik Lameter <dominik@lameter.com>
2020-08-08 15:53:54 -0500

    Added run data class header.

---
+++ run.h
@@ -0,0 +1,24 @@
+#ifndef RUN_H
+#define RUN_H
+
+#include <QDateTime>
+
+class Run {
+public:
+    Run(int, int, QString, QDateTime);
+
+    /* Access methods */
+    int getPlace();
+    int getTime();
+    QString getUser();
+    QDateTime getSubmittedDate();
+
+private:
+    /* Data members */
+    int m_place;
+    int m_time;
+    QString m_user;
+    QDateTime m_submittedDate;
+}
+
+#endif
