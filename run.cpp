4b8fb8c4971000c3145f9c754bb9cbdb520f69b6
Dominik Lameter <dominik@lameter.com>
2020-08-08 15:58:10 -0500

    Added definitions to data access function on Run data class.

--- run.cpp
+++ run.cpp
@@ -5,3 +5,19 @@ Run::Run(int place, int time, QString user, QDateTime submittedDate):
     m_time(time),
     m_user(user),
     m_submittedDate(submittedDate) {}
+
+int Run::getPlace() {
+    return m_place;
+}
+
+int Run::getTime() {
+    return m_time;
+}
+
+QString Run::getUser() {
+    return m_user;
+}
+
+QDateTime Run::getSubmittedDate() {
+    return m_submittedDate;
+}
